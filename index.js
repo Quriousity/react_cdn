c41a3ca82a80b1dd3930145bd41ceae17a310512
Qurious <32671466+bizzy91@users.noreply.github.com>
2022-11-23 14:33:11 +0900

    2. JSX

--- index.js
+++ index.js
@@ -2,7 +2,27 @@
 var root1 = ReactDOM.createRoot(document.getElementById('root1'));
 console.log(document.getElementById('hello_world'));
 root1.render(React.createElement(
-  'h1',
-  null,
-  'Hello, React!!!'
+    'h1',
+    null,
+    'Hello, React!!!'
 ));
+
+// 2, JSX
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+var user = {
+    firstName: 'Qurious',
+    lastName: 'Park'
+};
+
+var element = React.createElement(
+    'h1',
+    null,
+    'Hello, ',
+    formatName(user),
+    '!'
+);
+var root2 = ReactDOM.createRoot(document.getElementById('root2'));
+root2.render(element);
--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@
 const root1 = ReactDOM.createRoot(document.getElementById('root1'));
 console.log(document.getElementById('hello_world'));
 root1.render(<h1>Hello, React!!!</h1>);
+
+// 2, JSX
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+  
+const user = {
+    firstName: 'Qurious',
+    lastName: 'Park'
+};
+  
+const element = (
+    <h1>
+        Hello, {formatName(user)}!  
+    </h1>
+);
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+root2.render(element);
